d376b7bccda906894c202f26f8fe73106b73554a
Nialods <105827271+Nialods@users.noreply.github.com>
2024-02-26 19:08:08 -0300

    criando paginação e variáveis css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CodeEditorComponent } from './pages/code-editor/code-editor.component';
+import { CommunityComponent } from './pages/community/community.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'editor', pathMatch: 'full' },
+  { path: 'editor', component: CodeEditorComponent},
+  {path: 'community', component: CommunityComponent},
+  { path: '**', redirectTo: 'editor' }
+];
